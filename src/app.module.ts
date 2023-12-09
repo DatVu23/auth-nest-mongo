02afab2685fbf3006871f3f4fe45e772d008bbb0
DatVu23 <redb1997@gmail.com>
2023-12-09 21:46:35 +0300

    implemented signup & login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+			isGlobal: true,
+		}),
+		MongooseModule.forRoot(process.env.DB_URI, {
+			dbName: process.env.DB_NAME,
+		}),
+		AuthModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
